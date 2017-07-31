465f5c8c4dca497095346497634c99018291d490
Nandhini-Govindasamy <nandinigovi143@gmail.com>
2017-07-31 12:42:25 +0530

    new Business Object

--- src/main/java/com/primeforce/prodcast/businessobjects/NewCustomerRegistrationDetails.java
+++ src/main/java/com/primeforce/prodcast/businessobjects/NewCustomerRegistrationDetails.java
@@ -4,65 +4,119 @@ package com.primeforce.prodcast.businessobjects;
  * Created by SamayuSoftcorp on 16-03-2017.
  */
 public class NewCustomerRegistrationDetails {
-    private String Workphone,Firstname,Lastname,Email,Address1,Address2,Address3;
-    private String City,State,Country,Postalcode,Smsallowed,Cellphone;
-    private Long AccessId;
+    private String workPhone,firstName,lastName,email,address1,address2,address3;
+    private String city,state,country,postalCode,smsAllowed,cellPhone;
+    private long customerId;
 
+    public String getWorkPhone() {
+        return workPhone;
+    }
 
-    public String getworkPhone(){return Workphone;}
-    public void setWorkphone(String Workphone){
-         this.Workphone=Workphone;
-    };
-    public String getFirstname(){return Firstname;}
-    public void setFirstname(String Firstname){
-         this.Firstname=Firstname;
-    };
-    public String getLastname(){return Lastname;}
-    public void setLastname(String Lastname){
-        this.Lastname=Lastname;
-    };
-    public String getEmail(){return Email;}
-    public void setEmail(String Email){
-        this.Email=Email;
-    };
-    public String getAddress1(){return Address1;}
-    public void setAddress1(String Address1){
-        this.Address1=Address1;
-    };
-    public String getAddress2(){return Address2;}
-    public void setAddress2(String Address2){
-        this.Address2=Address2;
-    };
-    public String getAddress3(){return Address3;}
-    public void setAddress3(String Address3){
-        this.Address3=Address3;
-    };
-    public String getCity(){return City;}
-    public void setCity(String City){
-        this.City=City;
-    };
-    public String getState(){return State;}
-    public void setState(String State){
-        this.State=State;
-    };
-    public String getCountry(){return Country;}
-    public void setCountry(String Country){
-        this.Country=Country;
-    };
-    public String getPostalcode(){return Postalcode;}
-    public void setPostalcode(String Postalcode){
-        this.Postalcode=Postalcode;
-    };
-    public String getSmsallowed(){return Smsallowed;}
-    public void setSmsallowed(String Smsallowed){
-        this.Smsallowed=Smsallowed;
-    };
-   /* public long getAccessId(){return AccessId;}
-    public void setAccessId(long AccessId){
-        this.AccessId=AccessId;
-    };*/
-    public String getcellPhone(){return Cellphone;}
-    public void setCellphone(String Cellphone){
-        this.Cellphone=Cellphone;
-    };
+    public void setWorkPhone(String workPhone) {
+        this.workPhone = workPhone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getAddress3() {
+        return address3;
+    }
+
+    public void setAddress3(String address3) {
+        this.address3 = address3;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getSmsAllowed() {
+        return smsAllowed;
+    }
+
+    public void setSmsAllowed(String smsAllowed) {
+        this.smsAllowed = smsAllowed;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
 }
